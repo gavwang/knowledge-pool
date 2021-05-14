b210fbbdb1a6caed8bb6c82b712b8e914debbde4
Gavin Wang <gavin.wang@poly.com>
2021-05-14 08:56:34 +0800

    init knowledge repo

---
+++ cxx-basic/mymap.cpp
@@ -0,0 +1,16 @@
+/**
+ * This demostrate map class
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "This is my demostration for c++ basic" << endl;
+    return 0;
+}
